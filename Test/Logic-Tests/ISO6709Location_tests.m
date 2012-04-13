60b0d2d3c4ca342aaffdd579bfc5bfead0ee549d
Otto Schnurr <otto.schnurr@gmail.com>
2012-04-13 17:25:05 -0400

    ISO6709Location_stringFromCoordinate() passing all tests.

--- Test/Logic-Tests/ISO6709Location_tests.m
+++ Test/Logic-Tests/ISO6709Location_tests.m
@@ -18,24 +18,33 @@
 
 @implementation ISO6709Location_tests
 
-#pragma mark Factory Tests
+#pragma mark Synthesis Tests
 - (void)test_invalidCoordinate_isNilString
 {
    NSString* const locationString = ISO6709Location_stringFromCoordinate( kCLLocationCoordinate2DInvalid );
    STAssertNil( locationString, nil );
 }
 
+- (void)test_zeroCoordinate_hasExpectedString
+{
+   const CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake( .0, .0 );
+   NSString* const locationString = ISO6709Location_stringFromCoordinate( coordinate );
+   STAssertEqualObjects( locationString, @"+00.0000+000.0000/", nil );
+}
+
+- (void)test_typicalCoordinate_hasTypicalString
+{
+   const CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake( 12.345, -98.765 );
+   NSString* const locationString = ISO6709Location_stringFromCoordinate( coordinate );
+   STAssertEqualObjects( locationString, @"+12.3450-098.7650/", nil );
+}
+
+
+#pragma mark Parsing Tests
 - (void)test_nilString_isInvalidCoordinate
 {
    const CLLocationCoordinate2D coordinate = ISO6709Location_coordinateFromString( nil );
    STAssertFalse( CLLocationCoordinate2DIsValid( coordinate ), nil );
-}
-
-- (void)test_zeroCoordinate_hasExpectedString
-{
-   const CLLocationCoordinate2D coordinate = { 0 };
-   NSString* const locationString = ISO6709Location_stringFromCoordinate( coordinate );
-   STAssertEqualObjects( locationString, @"+00.0000+000.0000/", nil );
 }
 
 @end
